1d21702c13e1098aa9b237bfcc8c7da37bcc0fd9
Lise <35637366+luararamos@users.noreply.github.com>
2023-05-23 11:47:16 -0300

    viewBiding

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
@@ -2,10 +2,14 @@ package co.tiagoaguiar.tutorial.jokerappdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import co.tiagoaguiar.tutorial.jokerappdev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
