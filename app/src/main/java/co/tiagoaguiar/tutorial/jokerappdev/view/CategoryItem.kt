c9ba59b362c59960c29360cd9ebd23368a6eeded
Lise <35637366+luararamos@users.noreply.github.com>
2023-05-29 23:00:59 -0300

    Library Lisawray part 2

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/CategoryItem.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/CategoryItem.kt
@@ -1,22 +1,24 @@
 package co.tiagoaguiar.tutorial.jokerappdev.view
 
 import android.view.View
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.LinearLayout
+import android.widget.TextView
+import co.tiagoaguiar.tutorial.jokerappdev.R
 import co.tiagoaguiar.tutorial.jokerappdev.model.Category
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
 class CategoryItem(private val category: Category) : Item<CategoryItem.CategoryViewHolder>() {
-    class CategoryViewHolder(view: View): GroupieViewHolder(view){
+    class CategoryViewHolder(view: View) : GroupieViewHolder(view)
 
-    }
+    override fun createViewHolder(itemView: View) = CategoryViewHolder(itemView)
 
     override fun bind(viewHolder: CategoryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        viewHolder.itemView.findViewById<TextView>(R.id.txt_category).text = category.name
+        viewHolder.itemView.findViewById<LinearLayout>(R.id.container_category)
+            .setBackgroundColor(category.bgColor.toInt())
     }
 
-    override fun getLayout(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getLayout() = R.layout.item_category
 
 }
