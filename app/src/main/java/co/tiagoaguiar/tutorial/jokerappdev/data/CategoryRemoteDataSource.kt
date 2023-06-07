761e565d3628e68eb9f84bcb724c6c5f9fb45947
Lise <35637366+luararamos@users.noreply.github.com>
2023-06-07 16:07:29 -0300

    Create Retrofit

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
@@ -1,22 +1,36 @@
 package co.tiagoaguiar.tutorial.jokerappdev.data
 
-import android.os.Handler
-import android.os.Looper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CategoryRemoteDataSource {
+    fun findAllCategories(callback: ListCategoryCallback) {
+        HTTPClient.retrofit()
+            .create(ChuckNorrisAPI::class.java)
+            .findAllCategories()
+            .enqueue(object : Callback<List<String>> {
+                override fun onResponse(
+                    call: Call<List<String>>,
+                    response: Response<List<String>>
+                ) {
+                    if (response.isSuccessful) {
+                        val categories = response.body()
+                        callback.onSuccess(categories ?: emptyList())
+                    } else {
+                        // quando o servidor devolve status de error < 500
+                        val error = response.errorBody()?.string()
+                        callback.onError(error ?: "Erro desconhecido")
+                    }
+                    callback.onComplete()
+                }
 
-    fun findAllCategories(callback: ListCategoryCallback){
-        Handler(Looper.getMainLooper()).postDelayed({
-            val response = arrayListOf(
-                "Categoria 1",
-                "Categoria 2",
-                "Categoria 3",
-                "Categoria 4"
-            )
-            callback.onSuccess(response)
-//            callback.onError("FALHA NA CONEXÃO. TENTE NOVAMENTE MAIS TARDE")
-            callback.onComplete()
-        }, 2000)
+                override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                    callback.onError(t.message ?: "Erro interno")
+                    callback.onComplete()
+                }
+
+            })
 
     }
 
